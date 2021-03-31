ca43b6a4b45b1f76b89d26df1f4f432695728d2d
Gabriel Chaperon <gabrielchaperonb@gmail.com>
2021-03-31 15:56:24 -0300

    Formatting of shell scripts
    I took the liberty to format a little bit the shell scripts in data/ dir
    and also added python virtualenvs to gitignore.

--- data/preprocess.sh
+++ data/preprocess.sh
@@ -12,4 +12,16 @@ if [ ! -d $data_path ]; then
     fi
 fi
 cd $OpenNMT_py
-python3 preprocess.py -train_src $data_path/src-train.txt -train_tgt $data_path/tgt-train.txt -valid_src $data_path/src-val.txt -valid_tgt $data_path/tgt-val.txt -src_seq_length 1010 -tgt_seq_length 100 -src_vocab_size 1000 -tgt_vocab_size 1000 -dynamic_dict -share_vocab -save_data $data_path/final 2>&1 > $data_path/preprocess.out
+python3 preprocess.py \
+	-train_src $data_path/src-train.txt \
+	-train_tgt $data_path/tgt-train.txt \
+	-valid_src $data_path/src-val.txt \
+	-valid_tgt $data_path/tgt-val.txt \
+	-src_seq_length 1010 \
+	-tgt_seq_length 100 \
+	-src_vocab_size 1000 \
+	-tgt_vocab_size 1000 \
+	-dynamic_dict \
+	-share_vocab \
+	-save_data $data_path/final \
+	2>&1 > $data_path/preprocess.out
